e92f64fd15bc00f4ec1e574e29175be6a36361c7
Eric Huang <erichuang676@gmail.com>
2020-11-16 15:12:52 -0500

    Created MarketMovers and CompanyBoxGainers components

---
+++ src/components/screens/MarketMovers.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class MarketMovers extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Market Movers</Text>
+            </View>
+        )
+    }
+}
+
+export default MarketMovers
