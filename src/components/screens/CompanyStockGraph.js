56155794ba7720f34c061da5193e9bb7cc844b41
Eric Huang <erichuang676@gmail.com>
2020-12-04 11:35:07 -0500

    Created basic CompanyStockGraph component

---
+++ src/components/screens/CompanyStockGraph.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class CompanyStockGraph extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Graph </Text>
+            </View>
+        )
+    }
+}
