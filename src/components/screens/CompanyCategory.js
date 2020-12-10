12e70a30fb2a869c81206f0dd1fb62986fc26c9f
Eric Huang <erichuang676@gmail.com>
2020-12-10 10:40:16 -0500

    Created CompanyCategory component to display list of stocks from each category

---
+++ src/components/screens/CompanyCategory.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class CompanyCategory extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Stocks in category </Text>
+            </View>
+        )
+    }
+}
