fa6a286f3022f2089a9b7cba4e01eb429ff88377
Eric Huang <erichuang676@gmail.com>
2020-12-10 10:56:19 -0500

    Created CompanyCategoryBox component to display company stock info

---
+++ src/components/screens/CompanyCategoryBox.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class CompanyCategoryBox extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Individual stock </Text>
+            </View>
+        )
+    }
+}
