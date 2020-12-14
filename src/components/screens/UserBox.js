06aeb8530b997d71a28dbc5892f499d911fb8b15
Eric Huang <erichuang676@gmail.com>
2020-12-14 13:14:40 -0500

    Created basic UserList and UserBox components to display list of users

---
+++ src/components/screens/UserBox.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class UserBox extends Component {
+    render() {
+        return (
+            <View>
+                <Text> Individual User </Text>
+            </View>
+        )
+    }
+}
---
+++ src/components/screens/UserList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class UserList extends Component {
+    render() {
+        return (
+            <View>
+                <Text> List of people </Text>
+            </View>
+        )
+    }
+}
