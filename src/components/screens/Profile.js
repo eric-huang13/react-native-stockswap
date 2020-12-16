2aa40157afecedc8237fa0906d8d43e59f2d4bae
Eric Huang <erichuang676@gmail.com>
2020-12-16 11:56:09 -0500

    Set up basic structure of Profile component

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default class Profile extends Component {
     render() {
         return (
             <View>
-                <Text> textInComponent </Text>
+                <View>
+                <View>
+                <Text>Portfolio</Text>
+                <Text>Number</Text>
+                </View>
+                <View>
+                    <Text>Percentage</Text>
+                    <Text>Number</Text>
+                </View>
+                </View>
+                <View><Text>Graph</Text></View>
+                <View><Text>Buttons</Text></View>
+                <View>
+                <Text>Image</Text>
+                <Text> Name </Text>
+                </View>
+                <View><Text>Bio</Text></View>
+                <View><Text>Followers</Text><Text>Posts</Text><Text>Trades</Text><Text>Following</Text>
+                </View>
+                <View><Text>Buttons</Text></View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+
+    
+  });
+  
