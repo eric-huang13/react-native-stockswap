5bc123835a91a90f6b1b7d70dd5c227ec2efea6f
Eric Huang <erichuang676@gmail.com>
2021-01-04 13:25:57 -0500

    Created time filter buttons in userList component

--- src/components/screens/UserList.js
+++ src/components/screens/UserList.js
@@ -5,6 +5,7 @@ import {
   TouchableOpacity,
   ScrollView,
   StyleSheet,
+  Text,
 } from 'react-native';
 import {connect} from 'react-redux';
 import UserBox from './UserBox';
@@ -14,6 +15,13 @@ export class UserList extends Component {
     super(props);
     this.state = {
       input: '',
+      day: true,
+      week: false,
+      month: false,
+      threeMonth: false,
+      sixMonth:false,
+      year: false,
+      all: false,
     };
   }
   handleChange = (text) => {
@@ -35,6 +43,160 @@ export class UserList extends Component {
               placeholderTextColor="lightgrey"
               onChangeText={(text) => this.handleChange(text)}
             />
+          </View>
+          <Text style={style.timeFilterHeader}>Time filter:</Text>
+          <View style={style.timeFilterContainer}>
+          <TouchableOpacity
+              onPress={() =>
+                this.setState({
+          
+                  day: true,
+                  week: false,
+                  month: false,
+                  threeMonth: false,
+                  sixMonth:false,
+                  year: false,
+                  all: false,
+                })
+              }>
+              <Text
+                style={
+                  this.state.day
+                    ? {...style.timeFilterButtons, color: '#8b64ff'}
+                    : {...style.timeFilterButtons}
+                }>
+                1D</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() =>
+                this.setState({
+          
+                  day: false,
+                  week: true,
+                  month: false,
+                  threeMonth: false,
+                  sixMonth:false,
+                  year: false,
+                  all: false,
+                })
+              }>
+              <Text
+                style={
+                  this.state.week
+                    ? {...style.timeFilterButtons, color: '#8b64ff'}
+                    : {...style.timeFilterButtons}
+                }>
+                1W
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() =>
+                this.setState({
+          
+                  day: false,
+                  week: false,
+                  month: true,
+                  threeMonth: false,
+                  sixMonth:false,
+                  year: false,
+                  all: false,
+                })
+              }>
+              <Text
+                style={
+                  this.state.month
+                    ? {...style.timeFilterButtons, color: '#8b64ff'}
+                    : {...style.timeFilterButtons}
+                }>
+                1M
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+           onPress={() =>
+            this.setState({
+      
+              day: false,
+              week: false,
+              month: false,
+              threeMonth: true,
+              sixMonth:false,
+              year: false,
+              all: false,
+            })
+          }>
+              <Text
+                style={
+                  this.state.threeMonth
+                    ? {...style.timeFilterButtons, color: '#8b64ff'}
+                    : {...style.timeFilterButtons}
+                }>
+                
+                3M</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+           onPress={() =>
+            this.setState({
+      
+              day: false,
+              week: false,
+              month: false,
+              threeMonth: false,
+              sixMonth:true,
+              year: false,
+              all: false,
+            })
+          }>
+              <Text
+                style={
+                  this.state.sixMonth
+                    ? {...style.timeFilterButtons, color: '#8b64ff'}
+                    : {...style.timeFilterButtons}
+                }>
+                6M</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+           onPress={() =>
+            this.setState({
+      
+              day: false,
+              week: false,
+              month: false,
+              threeMonth: false,
+              sixMonth:false,
+              year: true,
+              all: false,
+            })
+          }>
+              <Text
+                style={
+                  this.state.year
+                    ? {...style.timeFilterButtons, color: '#8b64ff'}
+                    : {...style.timeFilterButtons}
+                }>
+                1Y</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+           onPress={() =>
+            this.setState({
+      
+              day: false,
+              week: false,
+              month: false,
+              threeMonth: false,
+              sixMonth:false,
+              year: false,
+              all: true,
+            })
+          }>
+              <Text
+                style={
+                  this.state.all
+                    ? {...style.timeFilterButtons, color: '#8b64ff'}
+                    : {...style.timeFilterButtons}
+                }>
+                All</Text>
+            </TouchableOpacity>
+
           </View>
           {filteredUsers.map((item) => {
             return (
@@ -78,5 +240,22 @@ const style = StyleSheet.create({
     height: 36,
     fontStyle: 'italic',
     paddingVertical: 0,
+  },
+  timeFilterHeader:{
+    fontSize:16,
+    paddingLeft:22,
+    color:'lightgrey'
+  },
+  timeFilterContainer: {
+    marginTop: 6,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginBottom: 10,
+    paddingBottom: 9,
+  },
+  timeFilterButtons: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
   },
 });
