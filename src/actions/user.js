b9b4ce95aa84cf71279749fe2f31040bc498d241
Eric Huang <erichuang676@gmail.com>
2021-01-11 15:41:24 -0500

    Created SignUp actions

--- src/actions/user.js
+++ src/actions/user.js
@@ -1,4 +1,24 @@
-import {LOGIN_SUCCESS, LOGOUT} from 'constants';
+import {LOGIN_SUCCESS, LOGOUT, SIGNUP_START, SIGNUP_SUCCESS, SIGNUP_ERROR} from 'constants';
+
+
+
+export const signUp = (input) => {
+  return (dispatch) => {
+    dispatch({type: SIGNUP_START});
+
+    axios
+    .post('https://jiujitsux.herokuapp.com/api/users/register', input)
+  
+    .then(response =>{ dispatch({ type: SIGNUP_SUCCESS, payload: response.data }) 
+})
+
+.catch(error => {dispatch({ type: SIGNUP_ERROR, payload: error.response })
+alert("Please try registering with a different username and password.")
+
+})
+  };
+};
+
 
 export const Login = () => (dispatch) => {
   return dispatch({
