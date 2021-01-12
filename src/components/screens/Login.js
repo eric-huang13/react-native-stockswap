605a24d11aaf7caa63c8ba562a0f57edcf0fb5a4
Eric Huang <erichuang676@gmail.com>
2021-01-12 15:22:44 -0800

    Android being included...

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -2,8 +2,18 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Button, SafeAreaView, Text} from 'react-native';
 import {Login} from 'actions/user';
+import axios from 'axios'
 
 class LoginScreen extends Component {
+  testAPI = () => {
+    fetch('http://10.0.2.2:9000')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => console.log(err))
+  }
+
   render() {
     const {isLoggedIn, LoginUser} = this.props;
 
@@ -11,6 +21,7 @@ class LoginScreen extends Component {
       <SafeAreaView>
         <Text>Is User Logged in: {'' + isLoggedIn} </Text>
         <Button title="Login Button" onPress={() => LoginUser()} />
+        <Button title="Test API" onPress={this.testAPI} />
       </SafeAreaView>
     );
   }
