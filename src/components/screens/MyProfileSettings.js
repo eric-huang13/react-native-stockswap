b30cd64e7e7ada43d3b4d35ea294759840e3e58e
Eric Huang <erichuang676@gmail.com>
2021-01-21 11:14:09 -0500

    Styled bottom of MyProfileSettings

--- src/components/screens/MyProfileSettings.js
+++ src/components/screens/MyProfileSettings.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native'
 
 export default class MyProfileSettings extends Component {
     render() {
         return (
-            <View style={style.container}>
+            <SafeAreaView style={style.container}>
+                <ScrollView>
                 <View style={style.topDetailsContainer}>
                     <View style={style.detailsRow}>
                     <View style={style.detailsColumn}>
@@ -48,9 +49,23 @@ export default class MyProfileSettings extends Component {
                 </View>
 
                 </View>
+                
+                <View style={style.bottomContainer}>
+                <View style={style.bottomInnerContiner}>
+                    <Text style={style.bottomText}>Privacy policy</Text>
+                    <Text style={style.detailsButtonChangeBottom}>Show</Text>
+                </View>
+                <View style={style.bottomInnerContiner}>
+                    <Text style={style.bottomText}>Terms and conditions</Text>
+                    <Text style={style.detailsButtonChangeBottom}>Show</Text>
+                </View>
 
-                <Text> textInComponent </Text>
-            </View>
+                </View>
+                <View style={style.logoutButtonContainer}>
+                <Text style={style.logoutButton}>Log Out</Text>
+                </View>
+                </ScrollView>
+            </SafeAreaView>
         )
     }
 }
@@ -128,7 +143,8 @@ const style = StyleSheet.create({
     middleDetailsText:{
     fontFamily:'Montserrat-Medium',
     color:'#FFFFFF',
-    fontSize:16
+    fontSize:16,
+    
         
     },
     notificationsContainer:{
@@ -136,6 +152,48 @@ const style = StyleSheet.create({
         justifyContent:'space-between',
         marginTop:30,
         marginBottom:10,
+
+    },
+    bottomContainer:{
+        paddingVertical:20,
+        paddingHorizontal:10,
+        borderBottomWidth:1,
+        borderBottomColor: 'rgba(158, 150, 150, .4)',
+  
+    },
+    bottomInnerContiner:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        paddingVertical:8,
+        marginBottom:8,
+    },
+    bottomText:{
+    fontFamily:'Montserrat-SemiBold',
+    color:'#FFFFFF',
+    fontSize:16,
+
+    },
+    detailsButtonChangeBottom:{
+        color: '#B8A0FF',
+        fontSize: 14,
+        fontFamily:'Montserrat-SemiBold',
+        marginLeft:24,  
+
+        },
+    logoutButtonContainer:{
+    marginTop:34,
+    marginHorizontal:20,
+    paddingVertical:10,
+    borderWidth:1,
+    borderColor:'#8B64FF',
+    borderRadius:6,    
+   },
+    logoutButton:{
+    textAlign:'center',
+    fontFamily:'Montserrat-SemiBold',
+    color:'#8B64FF',
+    fontSize:14,
+
 
     },
 })      
