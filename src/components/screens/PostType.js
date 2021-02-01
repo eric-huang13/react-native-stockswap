dec297bdaa22bfc286397f3cee7998a1fe08ac54
Eric Huang <erichuang676@gmail.com>
2021-02-01 16:58:13 -0500

    Created PostIcon and TradeIcon

--- src/components/screens/PostType.js
+++ src/components/screens/PostType.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet, TouchableOpacity, SafeAreaView } from 'react-native'
 
 export default class PostType extends Component {
     render() {
         return (
-            <View>
-                <Text> SELECT POST TYPE </Text>
-            </View>
+            <SafeAreaView style={style.mainContainer}>
+                <Text style={style.header}> SELECT POST TYPE </Text>
+            </SafeAreaView>
         )
     }
 }
+
+const style = StyleSheet.create({
+    mainContainer: {
+        flex:1,
+        backgroundColor:'#2a334a',
+        paddingVertical:20, 
+        paddingHorizontal:26,  
+        },
+        header:{
+            marginTop:20,
+            fontSize:20,
+            fontFamily: 'Montserrat-Bold',
+            color:'#FFFFFF', 
+            textAlign:'center',   
+        },
+    })
