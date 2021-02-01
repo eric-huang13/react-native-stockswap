51c7aa48ebb113412a67acaa205b3f5e8f54ec1b
Eric Huang <erichuang676@gmail.com>
2021-02-01 16:00:26 -0500

    Added styling and toggle switch to CreatePost

--- src/components/screens/CreatePost.js
+++ src/components/screens/CreatePost.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
-import { Text, View, StyleSheet, TextInput } from 'react-native'
+import { Text, View, StyleSheet, TextInput, SafeAreaView, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback,Switch, TouchableOpacity } from 'react-native'
+
 
 export default class CreatePost extends Component {
     constructor(props) {
         super(props);
     
         this.state = {
+          enabled: false,
           image:'',
           body:'',
         };
       }
+      
+      toggleSwitch = (value) => {
+        this.setState({enabled: value});
+      };
       handleBodyChange = (text) => {
         this.setState({
           body: text,
@@ -18,15 +24,21 @@ export default class CreatePost extends Component {
     render() {
         
         return (
-            <View>
-                <Text> Post a Publication </Text>
+        
+        
+              <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <SafeAreaView style={style.mainContainer}>
+            <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : null}
+          style={{flex: 1}}>
+                <Text style={style.header}> Post a Publication </Text>
                 <View style={style.uploadImageContainer}>
-                    <Text>Tap to upload cover image</Text>
+                    <Text style={style.uploadImageText}>Tap to upload cover image</Text>
                 </View>
-                <View>
+                <View style={style.postContainer}>
                   <Text style={style.inputHeader}>Post</Text>
                   <TextInput
-                    style={style.inputStyleBio}
+                    style={style.inputStyleBody}
                     value={this.state.body}
                     onChangeText={(text) => this.handleBodyChange(text)}
                     placeholder="Enter post text"
@@ -35,19 +47,132 @@ export default class CreatePost extends Component {
                     numberOfLines={4}
                   />
                 </View>
-                <View>
-                    <Text>Turn off comments button</Text>
-                </View>
-                <View>
-                    <Text>Buttons</Text>
-                </View>
+                
+                <View style={style.notificationsContainer}>
+              <Text style={style.middleDetailsText}>
+                Turn off notifications
+              </Text>
+              <Switch
+                trackColor={{false: '#1A2542', true: '#B8A0FF'}}
+                thumbColor={this.state.enabled ? '#f4f3f4' : '#f4f3f4'}
+                ios_backgroundColor="#f4f3f4"
+                onValueChange={this.toggleSwitch}
+                value={this.state.enabled}
+                style={{transform: [{scaleX: 1.5}, {scaleY: 1.5}]}}
+              />
             </View>
+            <View style={style.buttonsContainer}>
+                  <TouchableOpacity
+                    onPress={() => this.props.navigation.goBack()}>
+                    <Text style={style.publishButton}>Publish</Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => EditUserAccount(this.state)}>
+                    <Text style={style.saveButton}>Save</Text>
+                  </TouchableOpacity>
+                </View>
+                </KeyboardAvoidingView>
+            </SafeAreaView>
+            </TouchableWithoutFeedback>
+       
+            
         )
     }
 }
 
 const style = StyleSheet.create({
     mainContainer: {
-      
+        flex:1,
+        backgroundColor:'#2a334a',
+        paddingVertical:20, 
+        paddingHorizontal:26,  
+        },
+    header:{
+        fontSize:20,
+        fontFamily: 'Montserrat-Bold',
+        color:'#FFFFFF', 
+        textAlign:'center',   
     },
+    uploadImageContainer:{
+        marginTop:20,
+        backgroundColor:'#B8A0FF',
+        width:'100%',
+        alignSelf:'center',
+        height:100,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    uploadImageText:{
+        fontSize:14,
+        color:'#FFFFFF',
+        fontFamily: 'Montserrat-Regular',
+
+    },
+    postContainer:{
+        width:'100%',
+        alignSelf:'center',
+        marginTop:20,
+    },
+    inputHeader: {
+        fontSize: 12,
+        color: '#babec8',
+        marginBottom: 1,
+        fontFamily: 'Montserrat-Regular',
+      },
+      inputStyleBody: {
+        borderRadius: 6,
+        backgroundColor: '#3e4d6c',
+        marginBottom: 12,
+        padding: 8,
+        marginTop: 1,
+        fontSize: 14,
+        textAlignVertical: 'top',
+        fontFamily: 'Montserrat-Italic',
+        color: '#FFFFFF',
+        height:200,
+        
+      },
+      notificationsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 36,
+        marginBottom: 10,
+        // marginHorizontal:30,
+      },
+      middleDetailsText: {
+        fontFamily: 'Montserrat-Medium',
+        color: '#FFFFFF',
+        fontSize: 16,
+      },
+      buttonsContainer: {
+        marginTop: 28,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        // marginHorizontal:30,
+      },
+      publishButton: {
+        alignSelf: 'center',
+        color: '#8b64ff',
+        textAlign: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        width: 160,
+        borderRadius: 6,
+        fontSize: 14,
+        fontFamily: 'Montserrat-SemiBold',
+        borderWidth: 1,
+        borderColor: '#8b64ff',
+      },
+      saveButton: {
+        alignSelf: 'center',
+        backgroundColor: '#8b64ff',
+        color: '#FFFFFF',
+        textAlign: 'center',
+        paddingVertical: 12,
+        paddingHorizontal: 20,
+        width: 160,
+        borderRadius: 6,
+        fontSize: 14,
+        fontFamily: 'Montserrat-SemiBold',
+      },
 })
