ef9c09a36c36e02e3f836c579418cbac27ca82bd
Eric Huang <erichuang676@gmail.com>
2021-02-11 17:07:25 -0500

    Rendering FLatList of MyProfilePostBox inside LikedPosts

--- src/components/screens/MyProfilePostBox.js
+++ src/components/screens/MyProfilePostBox.js
@@ -12,31 +12,8 @@ import {connect} from 'react-redux';
  
  
 class MyProfilePosts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      shouldShow: false,
-      input: '',
 
-    };
-  } 
 
-  accountId = this.props.userAccount.id  
-
-  navigationByCondition = lastComment => {
-   const {navigation} = this.props;
-   if (lastComment.userId === this.accountId) {
-     navigation.navigate({
-       name: 'MyProfile',
-       params: {id: lastComment.id},
-     })
-   } else {
-     navigation.navigate({
-       name: 'Profile',
-       params: {id: lastComment.userId},
-     })
-   }
- };
   render() {
     const {comments, reply, userAccount, item} = this.props;
 
@@ -85,157 +62,11 @@ const style = StyleSheet.create({
     width: Dimensions.get('window').width / 3 - 2,
     margin: 1,    
   },
-  searchInputContainer: {
-    marginBottom: 15,
-  },
-  searchInput: {
-    paddingLeft: 40,
-    alignContent: 'center',
-    backgroundColor: '#3e4d6c',
-    color: 'lightgrey',
-    fontSize: 15,
-    height: 36,
-    fontFamily: 'Montserrat-Italic',
-    paddingVertical: 0,
-    marginBottom:10,
-  },
-  postContainer:{
-    paddingHorizontal: 10,
-  },
   image: {
     height: Dimensions.get('window').width / 3 - 2,
     width: '100%',
-    // borderRadius: 10,
   },
-  postNameContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 8.5,
-    justifyContent: 'space-between',
-  },
-  profileImageContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  postUserImage: {
-    height: 40,
-    width: 40,
-    borderRadius: 50,
-  },
-  postUserName: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    marginLeft: 8,
-    fontFamily:'Montserrat-Bold',
-  },
-  detailsContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginTop: 8,
-  },
-  likesContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-  },
-  timestamp: {
-    fontSize: 12.5,
-    color: 'lightgrey',
-    fontFamily:'Montserrat-Italic',
-  },
-  iconContainer:{
-    flexDirection:'row',
-     alignItems:'center',
-     justifyContent:'space-between',
-  },
-  likes: {
-    fontSize: 16,
-    color: 'lightgrey',
-    fontFamily:'Montserrat-Medium',
-    marginLeft:3,
-    marginRight:14,
-    
-  },
-  comments: {
-    fontSize: 16,
-    color: 'lightgrey',
-    fontFamily:'Montserrat-Medium',
-    marginRight: 1,
-    marginLeft:3,
-  },
-  body: {
-    fontSize: 15,
-    color: '#FFFFFF',
-    marginTop: 10,
-    marginBottom: 4,
-    fontFamily:'Montserrat-Medium',
-  },
-  more: {
-    fontSize: 13,
-    color: '#B8A0FF',
-    fontFamily:'Montserrat-SemiBoldItalic',
-  },
-  commentContainer: {
-    marginTop: 4,
-    paddingHorizontal:10,
-  },
-  allComments: {
-    color: '#8b64ff',
-    fontStyle: 'italic',
-    fontSize: 14,
-  },
-  lastCommentContainer: {
-    // marginTop: 1,
-  },
-  lastCommentName: {
-    color: '#999999',
-    fontFamily:'Montserrat-Bold',
-    marginBottom:1,
-  },
-  lastCommentBody: {
-    color: '#FFFFFF',
-    fontSize:13,
-    fontFamily:'Montserrat-Regular',
-  },
-  dotsDropdownContainer: {
-    flexDirection: 'column',
-    justifyContent: 'space-evenly',
-    alignContent: 'center',
-  },
-  dotsButton: {
-    alignSelf: 'flex-end',
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-  dropdown: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    width: '100%',
-    marginTop: 1,
-    marginBottom: -55,
-    backgroundColor: '#2C3957',
-    zIndex: 1,
-    paddingVertical: 8,
-    // paddingHorizontal:10,
-  },
-  dropDownText: {
-    color: 'white',
-    fontSize: 16,
-    marginHorizontal: 12,
-    fontFamily:'Montserrat-Medium',
-    paddingBottom:6,
-    paddingTop:6,
-  },
-  dropDownTextReportContainer: {
-    borderTopWidth: 1,
-    borderTopColor: '#CBCDD7',
-    paddingTop: 6,
-    paddingBottom:6,
-    backgroundColor:'#2C3957'
-  },
+ 
 });
  
 
