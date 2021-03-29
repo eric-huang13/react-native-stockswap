e0c4a421deb9a8e6fa850e2a4e30afa89761ad01
Eric Huang <erichuang676@gmail.com>
2021-03-29 16:29:54 -0400

    Added componentDidUpdate to MyProfilePostBox

--- src/components/screens/MyProfilePostBox.js
+++ src/components/screens/MyProfilePostBox.js
@@ -9,12 +9,39 @@ import {
 import {connect} from 'react-redux';
 
 class MyProfilePosts extends Component {
-  render() {
-    const {comments, reply, userAccount, item} = this.props;
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      filteredComments:[]
+    };
+  }
+   
+  componentDidMount() {
+    const {comments, item} = this.props;
     const filteredComments = comments.filter(
       (comment) => comment.postId === item.id,
     );
+    {
+        this.setState({
+          filteredComments: filteredComments,
+        });
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    const {comments, item} = this.props;
+    if (this.props.comments !== prevProps.comments) {
+      const filteredComments = comments.filter(
+        (comment) => comment.postId === item.id,
+      );
+      this.setState({
+        filteredComments: filteredComments,
+      });
+    }
+  }
+  render() {
+    const {reply, userAccount, item} = this.props;
 
     return (
       <SafeAreaView style={style.container}>
@@ -22,7 +49,7 @@ class MyProfilePosts extends Component {
           onPress={() =>
             this.props.navigation.navigate({
               name: 'PostScreen',
-              params: {post: item, filteredComments, reply, userAccount},
+              params: {post: item, filteredComments: this.state.filteredComments, reply, userAccount},
             })
           }>
           <Image style={style.image} source={{uri: item.img}} />
