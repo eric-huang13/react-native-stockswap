b8565fc43716af51d06006bc2e2678cd0aaf3283
Eric Huang <erichuang676@gmail.com>
2021-04-20 12:26:43 -0400

    Created responsiveFont file with functions for scaling app

---
+++ src/util/responsiveFont.js
@@ -0,0 +1,12 @@
+import { Dimensions } from 'react-native';
+const { width, height } = Dimensions.get('window');
+
+//Guideline sizes are based on pixel3a screen
+const guidelineBaseWidth = 411;
+const guidelineBaseHeight = 683;
+
+const scale = size => width / guidelineBaseWidth * size;
+const verticalScale = size => height / guidelineBaseHeight * size;
+const moderateScale = (size, factor = 0.5) => size + ( scale(size) - size ) * factor;
+
+export {scale, verticalScale, moderateScale};
