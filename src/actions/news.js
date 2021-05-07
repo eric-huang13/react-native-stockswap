1b405d7621f9309ad8501d2908a8af1bb77b2d1e
Eric Huang <erichuang676@gmail.com>
2021-05-07 08:10:03 -0400

    Set up news reducer

---
+++ src/actions/news.js
@@ -0,0 +1,27 @@
+import {
+    NEWS_FETCHING,
+    NEWS_SUCCESS,
+    NEWS_ERROR,
+    NEWS_STOP,
+  } from 'constants';
+  import axios from 'axios';
+ 
+  export const fetchNews = (page, offset) => {
+    return (dispatch) => {
+      dispatch({type: NEWS_FETCHING});  
+      axios
+        .get(`https://api.openbrewerydb.org/breweries?page=${page}&per_page=${offset}&by_state=ohio`)
+        // .then(response => console.log (response.data, "From NEWS API"))
+        .then((response) =>
+        response.data.length > 0 ?
+          dispatch({type: NEWS_SUCCESS, payload: response.data}) :
+          dispatch({type: NEWS_STOP, payload: false})
+        )
+        
+    
+        .catch((error) =>
+          dispatch({type: NEWS_ERROR, payload: error.response}),
+        );
+    };
+  };
+  
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,12 +6,14 @@ import userReducer from './user';
 import companyBoxReducer from './companyBoxReducer';
 import postsReducer from './postsReducer';
 import peopleReducer from './peopleReducer';
+import newsReducer from './newsReducer';
 
 const appReducer = combineReducers({
   user: userReducer,
   company: companyBoxReducer,
   posts: postsReducer,
   people: peopleReducer,
+  news: newsReducer,
 });
 
 const rootReducer = (state, action) => {
