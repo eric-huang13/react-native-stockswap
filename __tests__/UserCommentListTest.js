f0a80a0716e1820760e5b35122455b11a9a8542a
Eric Huang <erichuang676@gmail.com>
2021-05-20 12:04:14 -0400

    Created snapshot test for UserCommentList

---
+++ __tests__/UserCommentListTest.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import UserCommentList from '../src/components/HomeTabScreens/UserCommentList';
+import ShallowRenderer from 'react-test-renderer/shallow'; // ES6
+
+jest.mock('@react-native-community/google-signin', () => {});
+
+jest.mock("@react-native-async-storage/async-storage", () =>
+  require("@react-native-async-storage/async-storage/jest/async-storage-mock"),
+);
+jest.mock('react-redux', () => ({
+    connect: () => {
+      return (component) => {
+        return component
+      };
+    }
+  }));
+
+  test('renders correctly', () => {
+    const renderer = new ShallowRenderer();
+    renderer.render(<UserCommentList userAccount={{id:'test' }} route={{params: {params:{postId:'test',}}}} comments={[]} />);
+    const tree = renderer.getRenderOutput();
+    expect(tree).toMatchSnapshot();
+  });
